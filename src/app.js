d5c87648d9cff762d64a2466d647f97b6b43bdba
kadirbirdal <birdalkadir@outlook.com>
2021-10-25 02:43:21 +0300

    25 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba Dünya")
+
+let dolarDun = 9.55
+let dolarBugun = 9.75
+
+const turkLira = 1
+
+console.log(dolarBugun)
+console.log(dolarDun)
+console.log(turkLira)
+
+//Arrays
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredileri"]
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log(konutKredileri[i])
+    
+}
+
+
+console.log(konutKredileri)
